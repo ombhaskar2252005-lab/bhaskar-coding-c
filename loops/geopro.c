17804a00617b3c1816dcc563475fa4a93cf2eb12
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-01-04 15:19:33 +0530

    add files

---
+++ loops/geopro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    // 1, 2,4,8,16,32...
+    int a = 1;
+    for(int i=1; i<=n; i++){
+        printf("%d\n", a);
+        a = a * 2;
+    }
+}
