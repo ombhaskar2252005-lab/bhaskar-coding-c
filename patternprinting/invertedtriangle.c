d17e0791cb7d4cea74922043c2aedfb29f027ba7
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-01-07 01:42:54 +0530

    add files

---
+++ patternprinting/invertedtriangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(int i = n; i >= 1; i--){
+        for(int j = 1; j <= i; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+ }
