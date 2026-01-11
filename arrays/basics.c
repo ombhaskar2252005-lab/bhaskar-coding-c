044d40490940006b19749d4b7aadc9d3072c1868
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-01-11 19:33:11 +0530

    add files

---
+++ arrays/basics.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    int arr[5] = {1,3,5,2,6};
+    printf("first element:%d\n", arr[0]);
+    return 0;
+}
---
+++ arrays/loops1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int arr[5] = {2,3,1,4,6};
+    for(int i = 0; i<=4; i++){
+        printf("index %d = %d\n", i, arr[i]);
+    }
+    return 0;
+}
