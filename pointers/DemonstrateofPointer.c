15eb6b51593e993de57ec7ed71851746c40e6a7e
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-01-11 15:17:14 +0530

    add files

---
+++ pointers/DemonstrateofPointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+    int a = 10;
+    int *p;
+    p = &a;
+    printf("Value of a: %d\n", a);
+    printf("Address of a: %p\n", &a);
+    printf("Value stored in pointer p: %p\n", p);
+    printf("Value pointed to by p: %d\n", *p);
+    return 0;
+
+}
