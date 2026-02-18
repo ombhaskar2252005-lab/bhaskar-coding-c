5534f65c5d5031663e51c4566c447a060a3d8abe
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-02-18 21:57:17 +0530

    add files

---
+++ bhaskar.java
@@ -0,0 +1,7 @@
+public class bhaskar {
+    public static void main(String[] args){
+        System.out.print("i like pizza!\n");
+        System.out.println("i like actions movies");
+        System.out.print("BHASKAR IS MY NAME");
+    }
+}
